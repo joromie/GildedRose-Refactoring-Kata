c86f03794a5b3cc12c2ec8ca333cabc06e0f5859
Jeremy Lhuillier <jeremy.lhuillier.ext@careside.care>
2025-09-24 10:53:55 +0200

    ItemFactory - use registry

--- Java/src/main/java/com/gildedrose/ItemFactory.java
+++ Java/src/main/java/com/gildedrose/ItemFactory.java
@@ -1,12 +1,19 @@
 package com.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 class ItemFactory {
+    private static final Map<String, Function<Item, UpdatableItem>> registry = new HashMap<>();
+
+    static {
+        registry.put("Aged Brie", AgedBrieItem::new);
+        registry.put("Backstage passes to a TAFKAL80ETC concert", BackstagePassItem::new);
+        registry.put("Sulfuras, Hand of Ragnaros", SulfurasItem::new);
+    }
+
     public static UpdatableItem create(Item item) {
-        return switch (item.name) {
-            case "Aged Brie" -> new AgedBrieItem(item);
-            case "Backstage passes to a TAFKAL80ETC concert" -> new BackstagePassItem(item);
-            case "Sulfuras, Hand of Ragnaros" -> new SulfurasItem(item);
-            default -> new NormalItem(item);
-        };
+        return registry.getOrDefault(item.name, NormalItem::new).apply(item);
     }
 }
