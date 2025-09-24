db153aab80f1c493e79e851925c37e19fa4fe7ea
Jeremy Lhuillier <jeremy.lhuillier.ext@careside.care>
2025-09-24 10:37:25 +0200

    UpdatableItem - sellin and quality enhancements

--- Java/src/main/java/com/gildedrose/UpdatableItem.java
+++ Java/src/main/java/com/gildedrose/UpdatableItem.java
@@ -9,17 +9,34 @@ abstract class UpdatableItem {
 
     public abstract void update();
 
+    // Quality
     protected void increaseQuality() {
-        if (item.quality < 50) {
-            item.quality++;
-        }
+        if (item.quality < 50) item.quality++;
     }
 
     protected void decreaseQuality() {
-        if (item.quality > 0) {
-            item.quality--;
-        }
+        if (item.quality > 0) item.quality--;
     }
+
+    protected void decreaseQuality(int amount) {
+        for (int i = 0; i < amount; i++) decreaseQuality();
+    }
+
+    // Sell in
+    protected void decreaseSellIn() {
+        item.sellIn--;
+    }
+
+    protected boolean isExpired() {
+        return item.sellIn < 0;
+    }
+
+
+    protected void resetQuality() {
+        item.quality = 0;
+    }
+
+
 
     public Item getItem() {
         return item;
